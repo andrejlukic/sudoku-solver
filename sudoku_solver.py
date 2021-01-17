fdd86fef3007a546fee72ef5637edd918a1212b2
Andrej Lukic <kornflejks@gmail.com>
2021-01-17 11:07:24 +0100

    3x speed optimization by adding step 2 allowed moves constraint

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -2,7 +2,7 @@ import numpy as np
 import copy
 import time
 
-DBG = True
+DBG = False
 
 def log(s):
     if DBG:
@@ -13,7 +13,7 @@ def sudoku_rec(sudoku, history = None):
         for j in range(0, sudoku.shape[1]):
             if isinstance(sudoku[i,j], set):
                 for val in sudoku[i,j]:
-                    # log("Playing {0} from ({1},{2})".format(val,i,j))
+                    log("0 Playing {0} from ({1},{2})".format(val,i,j))
                     board_new, valid = update_permitted_states(sudoku, i, j, val)
 
                     if valid:
@@ -25,7 +25,8 @@ def sudoku_rec(sudoku, history = None):
                                 hist_new = copy.deepcopy(history)
                                 hist_new.append("Playing {0} from ({1},{2})".format(val,i,j))
                                 hist_new.append(board_new)
-                            # for b in hist_new:
+
+                            #for b in hist_new:
                             #    print("-------------")
                             #    print(b)
                             board_new, solved = sudoku_rec(board_new, history = hist_new)
@@ -41,9 +42,9 @@ def sudoku_solver(sudoku):
         return np.full((9, 9), -1)
     board = init_board(sudoku)
 
-    if not is_solved(board):
+    if not is_solved(board, skipvalidate=False):
         #history = [board]
-        return sudoku_rec(board)[0]
+        return sudoku_rec(board, history=None)[0]
     else:
         log("Solved by init")
         return board
@@ -51,12 +52,17 @@ def sudoku_solver(sudoku):
     return np.full((9, 9), -1)
 
 
-def is_solved(sudoku):
+def is_solved(sudoku, skipvalidate=True):
     for row in sudoku:
         for col in row:
             if isinstance(col, set) or col == -1:
                 return False
-    return is_valid(sudoku)
+    if skipvalidate:
+        return True
+    else:
+        return is_valid(sudoku)
+    #return True
+
 
 def is_valid(sudoku):
     # log(sudoku)
@@ -65,7 +71,7 @@ def is_valid(sudoku):
         for col in row:
             if col > 0:
                 if col in seen:
-                    # log("Row rule violated at {0}".format(row))
+                    log("Row rule violated at {0}".format(row))
                     return False
                 else:
                     seen.add(col)
@@ -75,7 +81,7 @@ def is_valid(sudoku):
         for row in col:
             if row > 0:
                 if row in seen:
-                    # log("Column rule violated at {0}".format(col))
+                    log("Column rule violated at {0}".format(col))
                     return False
                 else:
                     seen.add(row)
@@ -87,7 +93,7 @@ def is_valid(sudoku):
                 for j in range(3 * y, 3 * y + 3):
                     if sudoku[i,j] > 0:
                         if sudoku[i,j] in seen:
-                            # log("Quadrant rule violated at Q {0},{1}".format(x, y))
+                            log("Quadrant rule violated at Q {0},{1}".format(x, y))
                             return False
                         else:
                             seen.add(sudoku[i,j])
@@ -98,18 +104,25 @@ def update_permitted_states(board, i, j, value):
     new_state = copy.deepcopy(board)
     new_state[i,j] = value
 
+    second_step = []
     # update the row i
-    for s in new_state[i]:
-        if isinstance(s, set):
-            s.discard(value)
-            if len(s) == 0:
+    for y in range(0, new_state.shape[1]):
+        if isinstance(new_state[i,y], set):
+            new_state[i,y].discard(value)
+            if len(new_state[i,y]) == 1:
+                # log("ilegal state 1")
+                second_step.append((i,y, list(new_state[i,y])[0]))
+            elif len(new_state[i,y]) == 0:
                 # log("ilegal state 1")
                 return new_state, False
+
     # update the column j
-    for s in new_state.T[j]:
-        if isinstance(s, set):
-            s.discard(value)
-            if len(s) == 0:
+    for z in range(0, new_state.shape[0]):
+        if isinstance(new_state[z,j], set):
+            new_state[z,j].discard(value)
+            if len(new_state[z,j]) == 1:
+                second_step.append((z,j, list(new_state[z,j])[0]))
+            elif len(new_state[z,j]) == 0:
                 # log("ilegal state 2")
                 return new_state, False
 
@@ -122,9 +135,17 @@ def update_permitted_states(board, i, j, value):
                 for qj in range(3*q, 3*q + 3):
                     if isinstance(new_state[qi,qj], set):
                         new_state[qi,qj].discard(value)
-                        if len(new_state[qi,qj]) == 0:
+                        if len(new_state[qi,qj]) == 1:
+                            # log("ilegal state 3")
+                            second_step.append((qi,qj,list(new_state[qi,qj])[0]))
+                        elif len(new_state[qi,qj]) == 0:
                             # log("ilegal state 3")
                             return new_state, False
+
+    for step_two in second_step:
+        new_state, valid = update_permitted_states(new_state, step_two[0], step_two[1], step_two[2])
+        if not valid:
+            return new_state, False
     return new_state, True
 
 def init_board(sudoku):
